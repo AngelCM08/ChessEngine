0931a072e35d5ed250dc6bcf2891107865ebc067
AngelCM08 <angelcastromerino@gmail.com>
2023-04-14 19:08:43 +0200

    Mostrar ascii pruebas modulo y contar movimientos

--- projecte-final-dam/src/components/Tablero.js
+++ projecte-final-dam/src/components/Tablero.js
@@ -6,10 +6,14 @@ import { Chessboard } from "react-chessboard";
 export const Tablero = () => {
 
 	const [game, setGame] = useState(new Chess());
+	var numMovements;
 
 	function makeAMove(move) {
 		const gameCopy = { ...game };
+		//Por aquí debería haber una función que ponga colorines a los movimientos.
 		const result = gameCopy.move(move);
+		numMovements = game.history().length / 2;
+		console.log(numMovements);
 		setGame(gameCopy);
 		return result; // null if the move was illegal, the move object if the move was legal
 	}
@@ -36,7 +40,7 @@ export const Tablero = () => {
 
 	function getBestMove(fen) {
 		const game = new Chess(fen);
-		const depth = 4; // profundidad de búsqueda del árbol
+		const depth = 3; // profundidad de búsqueda del árbol
 		const alpha = -Infinity;
 		const beta = Infinity;
 		const isMaximizingPlayer = game.turn() === "w";
@@ -50,19 +54,25 @@ export const Tablero = () => {
 			/*Por ejemplo, queremos que nuestro rey esté alejado del centro en el medio juego. Sin embargo, como todos sabemos, 
 			es una pieza fundamental en los finales y mejor que esté en el centro. Para medir la fase de juego existente, 
 			los módulos pueden usar el nº de piezas sobre el tablero por ejemplo. */
-
+		
+		
 		//TODO 2. Pareja de alfiles: Se puede añadir un pequeño bonus por la pareja de alfiles (con la misma se cubren todas las casillas del tablero).
+
 
 		//TODO 3. Tablas de piezas y casillas: Son una manera simple de asignar valores a piezas específicas en casillas específicas. 
 			//Por ejemplo durante la apertura, los peones tendrán un pequeño bonus por ocupar casillas centrales.
 
+
 		//TODO 4. Seguridad del rey: Esto es muy importante. Por ejemplo se puede medir calculando la cantidad de peones que rodean al rey, o si hay una torre cerca del mismo.	
+
 
 		//TODO 5. Movilidad: Uno normalmente prefiere posiciones donde tienes más opciones, por ejemplo alfiles con diagonales abiertas, etc... 
 			//Esto se puede medir por ejemplo usando el nº de jugadas legales posibles en una posición como score para la movilidad.	
 
+
 		//TODO 6. Estructura de peones: Los peones doblados pueden dar un bonus negativo, 
 			//o por ejemplo los peones aislados en finales, ya que como todos sabemos son más fáciles de atacar. 
+
 
 		//TODO 7. Torre en columna abierta: Esto como sabemos suele ser positivo al igual que tener una torreen séptima. 
 			/*Hay muchos más factores que se pueden tener en cuenta pero estos siete resumen bien la idea de la evaluación 
@@ -88,6 +98,15 @@ export const Tablero = () => {
 				k: 0,
 			}[piece.type];
 
+			/*switch (piece.type) {
+				case value:
+					
+					break;
+			
+				default:
+					break;
+			}*/
+
 			if (piece.color === "w") {
 				whiteScore += pieceValue;
 			} else {
@@ -110,7 +129,7 @@ export const Tablero = () => {
 		for (let i = 0; i < possibleMoves.length; i++) {
 			game.move(possibleMoves[i]);
 			//Mostrar en tablero pequeño la posición actual que se está evaluando
-			console.log(game.board);
+			console.log(game.ascii());
 
 			//Evaluar siguiente posición
 			const [_, value] = minimax(depth - 1, alpha, beta, !isMaximizingPlayer);
