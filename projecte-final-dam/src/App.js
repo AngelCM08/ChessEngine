40d9e3426721c2c51f48613172702a512407f5c1
AngelCM08 <92088141+AngelCM08@users.noreply.github.com>
2023-05-01 14:24:04 +0200

    Starting style

--- projecte-final-dam/src/App.js
+++ projecte-final-dam/src/App.js
@@ -4,13 +4,20 @@ import { ModuleBoard } from './components/ModuleBoard';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <h1>TEST</h1>
-        <div className='tablero'>
+    <div className='App'>
+      <header className='App-header'>
+        <h1>Chess Module</h1>
+      </header>
+      <body className='App-body'>        
+        <div className='board'>
           <Board/>
         </div>
-      </header>
+        <div className='moduleBoard'>
+          <ModuleBoard/>
+        </div>
+        <div className='info'>
+        </div>
+      </body>
     </div>
   );
 }
